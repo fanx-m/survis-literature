9c0cdeab347a059f27846df5925d9e2725eced29
Frances <1924980677@qq.com>
2025-05-07 17:12:33 +0100

    Initial upload of SUrVis files

---
+++ data/generated/bib.js
@@ -0,0 +1,103 @@
+﻿define({ entries : {
+    "guo2024alphazero4cf": {
+        "author": "Guo, Yuxin",
+        "doi": "10.54254/2755-2721/33/20230228",
+        "journal": "Applied and Computational Engineering",
+        "title": "Implementing the AlphaZero Algorithm for Connect Four: A Deep Reinforcement Learning Approach",
+        "type": "article",
+        "year": "2024"
+    },
+    "kemmerling2023neuralmcts": {
+        "author": "Kemmerling, Marco and L\u00fctticke, Daniel and Schmitt, Robert H",
+        "doi": "10.1007/s10489-023-05240-w",
+        "journal": "Applied Intelligence",
+        "title": "Beyond Games: A Systematic Review of Neural Monte Carlo Tree Search Applications",
+        "type": "article",
+        "year": "2023"
+    },
+    "li2020drlsurvey": {
+        "author": "Li, Yuxi",
+        "doi": "10.48550/arXiv.2005.01338",
+        "journal": "arXiv preprint arXiv:2005.01338",
+        "title": "A Survey of Deep Reinforcement Learning in Video Games",
+        "type": "article",
+        "year": "2020"
+    },
+    "schrittwieser2020muzero": {
+        "author": "Schrittwieser, Julian and Antonoglou, Ioannis and Hubert, Thomas and Simonyan, Karen and Sifre, Laurent and Schmitt, Simon and Guez, Arthur and Lockhart, Edward and Hassabis, Demis and Silver, David",
+        "doi": "10.1038/s41586-020-03051-4",
+        "journal": "Nature",
+        "number": "7839",
+        "pages": "604--609",
+        "title": "Mastering Atari, Go, chess and shogi by planning with a learned model",
+        "type": "article",
+        "volume": "588",
+        "year": "2020"
+    },
+    "silver2016alphago": {
+        "author": "Silver, David and Huang, Aja and Maddison, Chris J and Guez, Arthur and Sifre, Laurent and van den Driessche, George and Schrittwieser, Julian and Antonoglou, Ioannis and Panneershelvam, Veda and Lanctot, Marc and others",
+        "doi": "10.1038/nature16961",
+        "journal": "Nature",
+        "number": "7587",
+        "pages": "484--489",
+        "publisher": "Nature Publishing Group",
+        "title": "Mastering the game of Go with deep neural networks and tree search",
+        "type": "article",
+        "volume": "529",
+        "year": "2016"
+    },
+    "silver2018alphazero": {
+        "author": "Silver, David and Schrittwieser, Julian and Simonyan, Karen and Antonoglou, Ioannis and Huang, Aja and Guez, Arthur and Hubert, Thomas and Baker, Lucas and Lai, Matthew and Bolton, Adrian and others",
+        "doi": "10.1126/science.aar6404",
+        "journal": "Science",
+        "number": "6419",
+        "pages": "1140--1144",
+        "title": "A general reinforcement learning algorithm that masters chess, shogi, and Go through self-play",
+        "type": "article",
+        "volume": "362",
+        "year": "2018"
+    },
+    "souchleris2023rlindustry": {
+        "author": "Souchleris, Konstantinos and Sidiropoulos, George K and Papakostas, George A",
+        "doi": "10.3390/app13042443",
+        "journal": "Applied Sciences",
+        "number": "4",
+        "pages": "2443",
+        "title": "Reinforcement Learning in Game Industry\u2014Review, Prospects and Challenges",
+        "type": "article",
+        "volume": "13",
+        "year": "2023"
+    },
+    "swiechowski2022mctsreview": {
+        "author": "'Swiechowski, Maciej and Godlewski, Konrad and Sawicki, Bartosz and Ma'n dziuk, Jacek",
+        "doi": "10.1007/s10462-022-10228-y",
+        "journal": "Artificial Intelligence Review",
+        "pages": "5291--5365",
+        "title": "Monte Carlo Tree Search: A Review of Recent Modifications and Applications",
+        "type": "article",
+        "volume": "55",
+        "year": "2022"
+    },
+    "toure2021minimaxvsrl": {
+        "author": "Tour\u00e9, Abdoul Wahab",
+        "doi": "10.1080/08839514.2021.1934265",
+        "journal": "Applied Artificial Intelligence",
+        "number": "8",
+        "pages": "602--619",
+        "title": "Performance Study of Minimax and Reinforcement Learning Agents in Board Games",
+        "type": "article",
+        "volume": "35",
+        "year": "2021"
+    },
+    "toure2023minimaxcf": {
+        "author": "Tour\u00e9, Abdoul Wahab",
+        "doi": "10.4236/am.2023.146025",
+        "journal": "Applied Mathematics",
+        "number": "6",
+        "pages": "289--303",
+        "title": "Evaluation of the Use of Minimax Search in Connect-4",
+        "type": "article",
+        "volume": "14",
+        "year": "2023"
+    }
+}});
